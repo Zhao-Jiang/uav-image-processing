4799ec31502ee297f4ee42ac5f6ce3c86328a349
自定义 <623028079@qq.com>
2019-09-08 15:03:36 +0800

    Create hyperExtract.py

---
+++ hyperExtract.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 15:11:37 2019
+
+Extracting average reflectancefor headwall-nano hyperspectral image.
+
+@author: jiangzhao
+"""
+
+import arcpy
+from arcpy.sa import *
+from arcpy import env
+import os
+
+ Data\2019\2019_Rice_Drought\headwall\0812hyper_temp\raw_17184_rd_rf_or1.tif'
+#ROI = r'Z:\Research Data\2019\2019_Rice_Drought\ROI\0812hyper17184.shp'
+rst = r'Z:\Rape_temporal\0424\6304.tif'
+ROI = r'Z:\Rape_temporal\BUFFER\buffer6304\XiaoNei.shp'
+ROI_num = 24
+env.workspace = r'Z:\Rape_temporal\results\analysis\0424hyper6304'
+
+os.mkdir(env.workspace)
+
+for band in range(1,272+1):
+    band_temp=rst+'\\Band_'+str(band)
+    print band_temp
+    ZonalStatisticsAsTable(ROI, "BianHao", band_temp, str(env.workspace)+'\\'+str(band)+'.dbf', "DATA", "ALL")
+
+import pandas as pd
+def dbfAssemble(dbfList=[]):
+    df=pd.DataFrame()
+    nCol=0
+    for table in dbfList:
+        print table
+        col_temp = range(ROI_num)
+        cursor = arcpy.da.SearchCursor(table, "MEAN")
+        nRow=0
+        for row in cursor:
+            col_temp[nRow] = row[0]
+            nRow += 1
+        colName = 'b_' + table[:-4] # band number ('b_125')
+        df.insert(nCol,colName,col_temp)
+        nCol += 1
+        
+    #write mean_reflectance_in_plot results (ROI_num plots*125bands)
+    writer = pd.ExcelWriter(str(env.workspace)+'\\hyper_mean_272bands.xlsx')
+#    df.T.to_excel(writer) # 转置得到 272*ROI_num
+    df.to_excel(writer) # 转置得到 272*ROI_num
+    writer.save()
+    writer.close()
+
+#dbfList = arcpy.ListTables('*')
+dbfList = range(272)
+for band in range(1,272+1):
+    dbfList[band-1] = env.workspace + '\\' + str(band) + '.dbf'
+dbfAssemble(dbfList)
