4512ca565f11e66aa3b1192a9f96618d869f87c7
自定义 <623028079@qq.com>
2019-09-12 09:58:25 +0800

    Update preprocess_forDeepLearning.py

--- preprocess_forDeepLearning.py
+++ preprocess_forDeepLearning.py
@@ -42,7 +42,7 @@ def imgPre(imgRaster, labelRaster, tileSize=(256,256), stride=64, saveTiles=True
         tile_rows = int((imgTif.RasterYSize-tileSize[0]+stride)/stride)
         tile_cols = int((imgTif.RasterXSize-tileSize[1]+stride)/stride)
         
-        imgArray = imgTif.ReadAsArray().swapaxes(0,1).swapaxes(1,2)
+        imgArray = imgTif.ReadAsArray().swapaxes(0,1).swapaxes(1,2)/255.0
 #        labelArray = (labelTif.ReadAsArray()/labelArray.max()).astype(np.uint8)
         labelArray = labelTif.ReadAsArray()
         
