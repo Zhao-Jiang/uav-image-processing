8da211a25cb35bc4252a6aae45f3f3f5bd73e3b9
自定义 <623028079@qq.com>
2019-09-25 15:28:54 +0800

    Update exg-exr_GreenLeafSeg.py

--- exg-exr_GreenLeafSeg.py
+++ exg-exr_GreenLeafSeg.py
@@ -53,45 +53,41 @@ for tif in list_tif:
 #        ExR=Nr*1.4-Ng
         ExG_ExR = (Ng*2 - Nr - Nb) - (Nr*1.4 - Ng) # =Ng*3 - Nr*2.4 - Nb
         
-        ExG_ExR_binary = np.zeros(ExG_ExR.shape).astype(np.uint8)
-        ExG_ExR_binary[ExG_ExR>=0] = 1
-        ExG_ExR_binary[Lightness<np.mean(Lightness)] = 0
+        ExG_ExR_binary = np.zeros(ExG_ExR.shape).astype(np.float32)
+        ExG_ExR_binary[ExG_ExR>=0] = 1.0
+        ExG_ExR_binary[Lightness<np.mean(Lightness)-np.std(Lightness)] = 0
         
-#         ExG_ExR_masked = ExG_ExR
-#         ExG_ExR_masked[ExG_ExR_binary==0] = 0
+#        ExG_ExR_masked = ExG_ExR
+#        ExG_ExR_masked[ExG_ExR_binary==0] = 0
         
-#         ##1 将计算好的VI保存为GeoTiff文件
-#         gtiff_driver = gdal.GetDriverByName('GTiff')
-#         # 批量处理需要注意文件名是变量，这里截取对应原始文件的不带后缀的文件名
-#         out_ds = gtiff_driver.Create(out_path + prename + '_ExG-ExR.tif',
-#                          ExG_ExR.shape[1], ExG_ExR.shape[0], 1, gdal.GDT_Float32)
-#         # 将NDVI数据坐标投影设置为原始坐标投影
-#         out_ds.SetProjection(in_ds.GetProjection())
-#         out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-#         out_band = out_ds.GetRasterBand(1)
-#         out_band.WriteArray(ExG_ExR)
-#         out_band.FlushCache()
+#        ##1 将计算好的VI保存为GeoTiff文件
+#        gtiff_driver = gdal.GetDriverByName('GTiff')
+#        # 批量处理需要注意文件名是变量，这里截取对应原始文件的不带后缀的文件名
+#        out_ds = gtiff_driver.Create(out_path + prename + '_ExG-ExR.tif',
+#                         ExG_ExR.shape[1], ExG_ExR.shape[0], 1, gdal.GDT_Float32)
+#        # 将坐标投影设置为原始坐标投影
+#        out_ds.SetProjection(in_ds.GetProjection())
+#        out_ds.SetGeoTransform(in_ds.GetGeoTransform())
+#        out_band = out_ds.GetRasterBand(1)
+#        out_band.WriteArray(ExG_ExR)
+#        out_band.FlushCache()
         
         ##2 将计算好的binary保存为GeoTiff文件
         gtiff_driver = gdal.GetDriverByName('GTiff')
-        # 批量处理需要注意文件名是变量，这里截取对应原始文件的不带后缀的文件名
         out_ds = gtiff_driver.Create(out_path + prename + '_ExG-ExR_binary.tif',
                          ExG_ExR_binary.shape[1], ExG_ExR_binary.shape[0], 1, gdal.GDT_Float32)
-        # 将NDVI数据坐标投影设置为原始坐标投影
         out_ds.SetProjection(in_ds.GetProjection())
         out_ds.SetGeoTransform(in_ds.GetGeoTransform())
         out_band = out_ds.GetRasterBand(1)
         out_band.WriteArray(ExG_ExR_binary)
         out_band.FlushCache()
         
-#         ##3 将计算好的masked vi保存为GeoTiff文件
-#         gtiff_driver = gdal.GetDriverByName('GTiff')
-#         # 批量处理需要注意文件名是变量，这里截取对应原始文件的不带后缀的文件名
-#         out_ds = gtiff_driver.Create(out_path + prename + '_ExG-ExR_masked.tif',
-#                          ExG_ExR_masked.shape[1], ExG_ExR_masked.shape[0], 1, gdal.GDT_Float32)
-#         # 将NDVI数据坐标投影设置为原始坐标投影
-#         out_ds.SetProjection(in_ds.GetProjection())
-#         out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-#         out_band = out_ds.GetRasterBand(1)
-#         out_band.WriteArray(ExG_ExR_masked)
-#         out_band.FlushCache()
+#        ##3 将计算好的masked vi保存为GeoTiff文件
+#        gtiff_driver = gdal.GetDriverByName('GTiff')
+#        out_ds = gtiff_driver.Create(out_path + prename + '_ExG-ExR_masked.tif',
+#                         ExG_ExR_masked.shape[1], ExG_ExR_masked.shape[0], 1, gdal.GDT_Float32)
+#        out_ds.SetProjection(in_ds.GetProjection())
+#        out_ds.SetGeoTransform(in_ds.GetGeoTransform())
+#        out_band = out_ds.GetRasterBand(1)
+#        out_band.WriteArray(ExG_ExR_masked)
+#        out_band.FlushCache()
